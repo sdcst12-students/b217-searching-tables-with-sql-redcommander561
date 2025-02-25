558e6ff34f2523b12756b763d4add74f2db5bf51
redcommander561 <jaysona803@deltalearns.ca>
2025-02-25 10:27:26 -0800

    .
    .

--- question-answer.py
+++ question-answer.py
@@ -37,14 +37,41 @@ knight_count = len(knight)
 print("Number of Knights:", knight_count)
 
 #class with highest members
-cursor.execute("""
-    select class, count(*)
-    from npc
-    group by class
-    order by count(*) desc
-    limit 1
-""")
+cursor.execute("select class, count(*) from npc group by class order by count(*) desc limit 1")
 members = cursor.fetchall()
 print("Class with highest members:", members)
 
 #ID num of jester with most gold
+cursor.execute(" select id from npc where class = 'Jester' order by gold desc limit 1")
+jester = cursor.fetchall()
+print("ID of Jester with the most gold:", jester)
+#total gold of the 100 wealthiest npc's in the table
+cursor.execute("select sum(gold) from (select gold from npc order by gold desc limit 100)")
+numGold = cursor.fetchall()
+print("total gold of 100 wealthiest NPC's in the table is: ", numGold)
+
+#total gold of the 100 wealthiest npc's under level 5
+cursor.execute("select sum(gold) from npc where level < 5 order by gold desc limit 100")
+numLVL5 = cursor.fetchall() 
+print("Total gold of 100 wealthiest NPC's under level 5: ", numLVL5)
+
+#stats of the Bard with the highest strength
+cursor.execute("select * from npc where class = 'Bard' order by strength desc limit 1")
+fard = cursor.fetchall()
+print("stats of the bard with the highest strength: ", fard)
+
+#ID number of the npc with highest total sum of their 6 primary stats
+cursor.execute(" select id from npc order by (strength + dexterity + constitution + intelligence + wisdom + charisma) desc limit 1")
+primbyaccount = cursor.fetchall()
+print("ID number of the NPC with the highest total sum of their 6 primary stats: ", primbyaccount)
+
+#percentage of all fighter classes (Barbarian, Warrior, Knight, Samurai) are Warriors
+cursor.execute("select(select count(*) from npc where class = 'Warrior') * 100.0 /(select count(*) from npc where class in ('Barbarian', 'Warrior', 'Knight', 'Samurai'))")
+fightclass = cursor.fetchall()
+print("percentage of all fighter classes are warriors: ", fightclass)
+
+#average hitpoints per level of the npc's that are level 10 or higher
+cursor.execute("select avg(hp) from npc where level >= 10")
+hp = cursor.fetchall()
+print("average hitpoints per level of the NPC's that are level 10 or higher: ", hp)
+
